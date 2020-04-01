e251be0e2e998170025bbdce46684eac06cd4956
SerhiyKr <sergiokrupnik@gmail.com>
2020-04-01 23:42:28 +0300

    dialog page

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,23 @@ import s from './Dialogs.module.css';
 
 const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog + ' ' + s.active}>
+                    Serhiy
+                </div>
+                <div className={s.dialog}>
+                    Georgie
+                </div>
+                <div className={s.dialog}>
+                    Pupa
+                </div>
+            </div>
+            <div className="messages">
+                <div className={s.message}>Hiiii</div>
+                <div className={s.message}>How are you?</div>
+                <div className={s.message}>Fuck youuu...</div>
+            </div>
         </div>
     )
 }
